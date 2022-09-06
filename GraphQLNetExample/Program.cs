b374fc0f13449620f48b8947afeac3962730a9cc
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-09-06 23:02:01 +0700

    Remove unused and update code to support breaking changes

--- GraphQLNetExample/Program.cs
+++ GraphQLNetExample/Program.cs
@@ -1,6 +1,5 @@
+using GraphQL;
 using GraphQL.MicrosoftDI;
-using GraphQL.Server;
-using GraphQL.SystemTextJson;
 using GraphQL.Types;
 using GraphQLNetExample.EntityFramework;
 using GraphQLNetExample.Notes;
@@ -15,10 +14,8 @@ builder.Services.AddDbContext<NotesContext>(options =>
 });
 builder.Services.AddSingleton<ISchema, NotesSchema>(services => new NotesSchema(new SelfActivatingServiceProvider(services)));
 builder.Services.AddGraphQL(options =>
-                {
-                    options.EnableMetrics = true;
-                })
-                .AddSystemTextJson();
+                    options.AddSystemTextJson()
+                );
 
 builder.Services.AddCors(options =>
 {
@@ -54,6 +51,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.UseGraphQL<ISchema>();
+app.UseGraphQL();
 
 app.Run();
